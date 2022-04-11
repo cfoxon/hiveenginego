b789bea9b9794fc440a5bc23a57cd2ed8a5ce751
cfoxon <chuckfoxon@gmail.com>
2022-04-11 08:41:38 -0600

    Added error handling for JSON unmarshalling

--- fungibles.go
+++ fungibles.go
@@ -1,129 +1,126 @@
 package hiveenginego
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
+	"encoding/json"
 )
 
+/*
 type tokenArray struct {
-    tokArray []HiveEngineFungibleToken
+	tokArray []HiveEngineFungibleToken
 }
+*/
 
 type HiveEngineFungibleToken struct {
-    FungibleId           int             `json:"_id"`
-    Issuer               string          `json:"issuer"`
-    Symbol               string          `json:"symbol"`
-    Name                 string          `json:"name"`
-    MetaData             json.RawMessage `json:"metadata"`
-    DelegationEnabled    bool            `json:"delegationEnabled"`
-    Precision            int             `json:"precision"`
-    StakingEnabled       bool            `json:"stakingEnabled"`
-    UndelegationCooldown int             `json:"undelegationCooldown"`
-    UnstakingCooldown    int             `json:"unstakingCooldown"`
+	FungibleId           int             `json:"_id"`
+	Issuer               string          `json:"issuer"`
+	Symbol               string          `json:"symbol"`
+	Name                 string          `json:"name"`
+	MetaData             json.RawMessage `json:"metadata"`
+	DelegationEnabled    bool            `json:"delegationEnabled"`
+	Precision            int             `json:"precision"`
+	StakingEnabled       bool            `json:"stakingEnabled"`
+	UndelegationCooldown int             `json:"undelegationCooldown"`
+	UnstakingCooldown    int             `json:"unstakingCooldown"`
 }
 
 type FungibleBalance struct {
-    Id int `json:"_id"`
-    Account string `json:"account"`
-    Symbol string `json:"symbol"`
-    Balance string `json:"balance"`
-    Stake string `json:"stake"`
-    PendingUnstake string `json:"pendingUnstake"`
-    DelegationsIn string `json:"delegationsIn"`
-    DelegationsOut string `json:"delegationsOut"`
-    PendingUndelegations string `json:"pendingUndelegations"`
+	Id                   int    `json:"_id"`
+	Account              string `json:"account"`
+	Symbol               string `json:"symbol"`
+	Balance              string `json:"balance"`
+	Stake                string `json:"stake"`
+	PendingUnstake       string `json:"pendingUnstake"`
+	DelegationsIn        string `json:"delegationsIn"`
+	DelegationsOut       string `json:"delegationsOut"`
+	PendingUndelegations string `json:"pendingUndelegations"`
 }
 
-type FungibleTokenTransfer struct  {
-    Symbol string `json:"symbol"`
-    To string `json:"to"`
-    Quantity string `json:"quantity"`
-    Memo string `json:"memo"`
+type FungibleTokenTransfer struct {
+	Symbol   string `json:"symbol"`
+	To       string `json:"to"`
+	Quantity string `json:"quantity"`
+	Memo     string `json:"memo"`
 }
 
-func CreateFungibleTokenTransfer(symbol string, to string, quantity string, memo string) ContractTx{
-    tokenTransfer := FungibleTokenTransfer{
-        Symbol: symbol,
-        To: to,
-        Quantity: quantity,
-        Memo: memo,
-    }
-    contractTrx := ContractTx{
-        ContractName: "tokens",
-        ContractAction: "transfer",
-        ContractPayload: tokenTransfer,
-    }
-    return contractTrx
+func CreateFungibleTokenTransfer(symbol string, to string, quantity string, memo string) ContractTx {
+	tokenTransfer := FungibleTokenTransfer{
+		Symbol:   symbol,
+		To:       to,
+		Quantity: quantity,
+		Memo:     memo,
+	}
+	contractTrx := ContractTx{
+		ContractName:    "tokens",
+		ContractAction:  "transfer",
+		ContractPayload: tokenTransfer,
+	}
+	return contractTrx
 }
 
 func (h HiveEngineRpcNode) getFungibleTokenCount() (int, error) {
-    if len(h.Endpoints.Contracts) == 0 {
-        h.Endpoints.Contracts = "/contracts"
-    }
-    endpoint := h.RpcNode.address + h.Endpoints.Contracts
-    qParamsIndex := []ContractQueryParamsIndex{}
-    qParamsIndex = append(qParamsIndex, ContractQueryParamsIndex{Index: "_id", Descending: true})
-    qParams := ContractQueryParams{Contract: "tokens", Table: "tokens", Limit: 1, Offset: 0, Index: qParamsIndex}
-    query := herpcQuery{method: "find", params: qParams}
+	if len(h.Endpoints.Contracts) == 0 {
+		h.Endpoints.Contracts = "/contracts"
+	}
+	endpoint := h.RpcNode.address + h.Endpoints.Contracts
+	qParamsIndex := []ContractQueryParamsIndex{}
+	qParamsIndex = append(qParamsIndex, ContractQueryParamsIndex{Index: "_id", Descending: true})
+	qParams := ContractQueryParams{Contract: "tokens", Table: "tokens", Limit: 1, Offset: 0, Index: qParamsIndex}
+	query := herpcQuery{method: "find", params: qParams}
 
-    res, err := h.rpcExec(endpoint, query)
-    if err != nil {
-        return 0, err
-    }
+	res, err := h.rpcExec(endpoint, query)
+	if err != nil {
+		return 0, err
+	}
 
-    token := []HiveEngineFungibleToken{}
-    if err := json.Unmarshal(res, &token); err != nil {
-        log.Println("Can not unmarshal result for fungible token count")
-        log.Println(err)
-        log.Println(res)
-    }
+	token := []HiveEngineFungibleToken{}
+	if err := json.Unmarshal(res, &token); err != nil {
+		return 0, err
+	}
 
-    countSymbolNFT := token[0].FungibleId
+	countSymbolNFT := token[0].FungibleId
 
-    return countSymbolNFT, nil
+	return countSymbolNFT, nil
 }
 
 func (h HiveEngineRpcNode) GetAllFungibleTokens() ([]HiveEngineFungibleToken, error) {
-    totalTokens, err := h.getFungibleTokenCount()
-    if err != nil {
-        return nil, err
-    }
+	totalTokens, err := h.getFungibleTokenCount()
+	if err != nil {
+		return nil, err
+	}
 
-    offsetsNeeded := totalTokens / 1000
+	offsetsNeeded := totalTokens / 1000
 
-    qParamsIndex := []ContractQueryParamsIndex{}
+	qParamsIndex := []ContractQueryParamsIndex{}
 
-    var queries []herpcQuery
+	var queries []herpcQuery
 
-    for i := 0; i <= offsetsNeeded; i++ {
-        offset := i * 1000
-        qParams := ContractQueryParams{Contract: "tokens", Table: "tokens", Limit: 1000, Offset: offset, Index: qParamsIndex}
-        query := herpcQuery{method: "find", params: qParams}
-        queries = append(queries, query)
-    }
+	for i := 0; i <= offsetsNeeded; i++ {
+		offset := i * 1000
+		qParams := ContractQueryParams{Contract: "tokens", Table: "tokens", Limit: 1000, Offset: offset, Index: qParamsIndex}
+		query := herpcQuery{method: "find", params: qParams}
+		queries = append(queries, query)
+	}
 
-    tokens := []HiveEngineFungibleToken{}
+	tokens := []HiveEngineFungibleToken{}
 
-    if len(queries) > 0 {
-        if len(h.Endpoints.Contracts) == 0 {
-            h.Endpoints.Contracts = "/contracts"
-        }
-        endpoint := h.RpcNode.address + h.Endpoints.Contracts
-        ress, err := h.rpcExecBatch(endpoint, queries)
-        if err != nil {
-            log.Println(err)
-        }
-        var batchResult []HiveEngineFungibleToken
-        for _, res := range ress {
-            thisresult := []HiveEngineFungibleToken{}
-            if err := json.Unmarshal(res, &thisresult); err != nil { // Parse []byte to the go struct pointer
-                fmt.Println(string(res), "Can not unmarshal fungibles from response result")
-                fmt.Println(err)
-            }
-            batchResult = append(tokens, thisresult...)
-        }
-        tokens = append(tokens, batchResult...)
-    }
-    return tokens, nil
+	if len(queries) > 0 {
+		if len(h.Endpoints.Contracts) == 0 {
+			h.Endpoints.Contracts = "/contracts"
+		}
+		endpoint := h.RpcNode.address + h.Endpoints.Contracts
+		ress, err := h.rpcExecBatch(endpoint, queries)
+		if err != nil {
+			return nil, err
+		}
+		var batchResult []HiveEngineFungibleToken
+		for _, res := range ress {
+			thisresult := []HiveEngineFungibleToken{}
+			if err := json.Unmarshal(res, &thisresult); err != nil { // Parse []byte to the go struct pointer
+				return nil, err
+			}
+			batchResult = append(tokens, thisresult...)
+		}
+		tokens = append(tokens, batchResult...)
+	}
+	return tokens, nil
 }
--- nft.go
+++ nft.go
@@ -1,169 +1,171 @@
 package hiveenginego
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "strconv"
-    "strings"
+	"encoding/json"
+	"strconv"
+	"strings"
 )
 
 type EngineNft struct {
-    Id              int             `json:"_id"`
-    Account         string          `json:"account"`
-    OwnedBy         string          `json:"ownedBy"`
-    LockedTokens    json.RawMessage `json:"lockedTokens"`
-    Properties      json.RawMessage `json:"properties"`
-    PreviousAccount string          `json:"previousAccount"`
-    PreviousOwnedBy string          `json:"previousOwnedBy"`
+	Id              int             `json:"_id"`
+	Account         string          `json:"account"`
+	OwnedBy         string          `json:"ownedBy"`
+	LockedTokens    json.RawMessage `json:"lockedTokens"`
+	Properties      json.RawMessage `json:"properties"`
+	PreviousAccount string          `json:"previousAccount"`
+	PreviousOwnedBy string          `json:"previousOwnedBy"`
 }
 
 type NftTransferPayload struct {
-    Nfts []NftsForNftTransfer `json:"nfts"`
-    To string `json:"to"`
-    Memo string `json:"memo,omitempty"`
+	Nfts []NftsForNftTransfer `json:"nfts"`
+	To   string               `json:"to"`
+	Memo string               `json:"memo,omitempty"`
 }
 
 type NftsForNftTransfer struct {
-    Symbol string `json:"symbol"`
-    Ids []string `json:"ids"`
+	Symbol string   `json:"symbol"`
+	Ids    []string `json:"ids"`
 }
 
-func CreateNftTransfer (symbol string, nftIds []int, to string, memo string) []ContractTx {
-    var nftIdsStr []string
-    for _, nftId := range nftIds {
-        nftIdsStr = append(nftIdsStr, strconv.Itoa(nftId))
-    }
-    var preppedTx []ContractTx
-    if len(nftIdsStr) > 50 {
-        for i := 0; i <= len(nftIdsStr); {
-            var l int
-            if i+50 > len(nftIdsStr) {
-                l = len(nftIdsStr)
-            } else {
-                l = i + 50
-            }
-            strid := nftIdsStr[i:l]
-            nftTransfer := NftsForNftTransfer{Symbol: symbol, Ids: strid}
-            payload := NftTransferPayload{Nfts: []NftsForNftTransfer{nftTransfer}, To: to, Memo: memo}
-            contrx := ContractTx{ContractName: "nft", ContractAction: "transfer", ContractPayload: payload}
-            preppedTx = append(preppedTx, contrx)
-            i += 50
-        }
-    } else {
-        nftTransfer := NftsForNftTransfer{Symbol: symbol, Ids: nftIdsStr}
-        payload := NftTransferPayload{Nfts: []NftsForNftTransfer{nftTransfer}, To: to, Memo: memo}
-        contrx := ContractTx{ContractName: "nft", ContractAction: "transfer", ContractPayload: payload}
-        preppedTx = append(preppedTx, contrx)
-    }
-    return preppedTx
+func CreateNftTransfer(symbol string, nftIds []int, to string, memo string) []ContractTx {
+	var nftIdsStr []string
+	for _, nftId := range nftIds {
+		nftIdsStr = append(nftIdsStr, strconv.Itoa(nftId))
+	}
+	var preppedTx []ContractTx
+	if len(nftIdsStr) > 50 {
+		for i := 0; i <= len(nftIdsStr); {
+			var l int
+			if i+50 > len(nftIdsStr) {
+				l = len(nftIdsStr)
+			} else {
+				l = i + 50
+			}
+			strid := nftIdsStr[i:l]
+			nftTransfer := NftsForNftTransfer{Symbol: symbol, Ids: strid}
+			payload := NftTransferPayload{Nfts: []NftsForNftTransfer{nftTransfer}, To: to, Memo: memo}
+			contrx := ContractTx{ContractName: "nft", ContractAction: "transfer", ContractPayload: payload}
+			preppedTx = append(preppedTx, contrx)
+			i += 50
+		}
+	} else {
+		nftTransfer := NftsForNftTransfer{Symbol: symbol, Ids: nftIdsStr}
+		payload := NftTransferPayload{Nfts: []NftsForNftTransfer{nftTransfer}, To: to, Memo: memo}
+		contrx := ContractTx{ContractName: "nft", ContractAction: "transfer", ContractPayload: payload}
+		preppedTx = append(preppedTx, contrx)
+	}
+	return preppedTx
 }
 
-func (h HiveEngineRpcNode) getSymbolNFTCount(nftSymbol string) int {
-    nftSymbolUpper := strings.ToUpper(nftSymbol)
-    if len(h.Endpoints.Contracts) == 0 {
-        h.Endpoints.Contracts = "/contracts"
-    }
-    endpoint := h.Endpoints.Contracts
+func (h HiveEngineRpcNode) getSymbolNFTCount(nftSymbol string) (int, error) {
+	nftSymbolUpper := strings.ToUpper(nftSymbol)
+	if len(h.Endpoints.Contracts) == 0 {
+		h.Endpoints.Contracts = "/contracts"
+	}
+	endpoint := h.Endpoints.Contracts
 
-    qParamsIndex := []ContractQueryParamsIndex{ContractQueryParamsIndex{Index: "_id", Descending: true}}
-    qParams := ContractQueryParams{Contract: "nft", Table: nftSymbolUpper + "instances",Query: struct {}{}, Limit: 1, Offset: 0, Index: qParamsIndex}
-    query := herpcQuery{method: "find", params: qParams}
+	qParamsIndex := []ContractQueryParamsIndex{{Index: "_id", Descending: true}}
+	qParams := ContractQueryParams{Contract: "nft", Table: nftSymbolUpper + "instances", Query: struct{}{}, Limit: 1, Offset: 0, Index: qParamsIndex}
+	query := herpcQuery{method: "find", params: qParams}
 
-    res, err := h.rpcExec(endpoint, query)
-    if err != nil {
-        log.Println(err)
-    }
+	res, err := h.rpcExec(endpoint, query)
+	if err != nil {
+		return 0, err
+	}
 
-    nft := []EngineNft{}
+	nft := []EngineNft{}
 
-    if err := json.Unmarshal(res, &nft); err != nil {
-        fmt.Println("Can not unmarshal result for nft count")
-        fmt.Println(err)
-        fmt.Println(res)
-    }
+	if err := json.Unmarshal(res, &nft); err != nil {
+		return 0, nil
+	}
 
-    countSymbolNFT := nft[0].Id
+	countSymbolNFT := nft[0].Id
 
-    return countSymbolNFT
+	return countSymbolNFT, nil
 }
 
 func (h HiveEngineRpcNode) GetSymbolAllNft(nftSymbol string) ([]EngineNft, error) {
-    if len(h.Endpoints.Contracts) == 0 {
-        h.Endpoints.Contracts = "/contracts"
-    }
-    if h.RpcNode.MaxConn == 0 {
-        h.RpcNode.MaxConn = 1
-    }
-    if h.RpcNode.MaxBatch == 0 {
-        h.RpcNode.MaxBatch = 2
-    }
+	if len(h.Endpoints.Contracts) == 0 {
+		h.Endpoints.Contracts = "/contracts"
+	}
+	if h.RpcNode.MaxConn == 0 {
+		h.RpcNode.MaxConn = 1
+	}
+	if h.RpcNode.MaxBatch == 0 {
+		h.RpcNode.MaxBatch = 2
+	}
 
-    nftSymbolUpper := strings.ToUpper(nftSymbol)
-    collectionSize := h.getSymbolNFTCount(nftSymbolUpper)
-    offsetsNeeded := collectionSize / 1000
+	nftSymbolUpper := strings.ToUpper(nftSymbol)
+	collectionSize, err := h.getSymbolNFTCount(nftSymbolUpper)
+	if err != nil {
+		return nil, err
+	}
+	offsetsNeeded := collectionSize / 1000
 
-    qParamsIndex := []ContractQueryParamsIndex{}
+	qParamsIndex := []ContractQueryParamsIndex{}
 
-    var queries []herpcQuery
-    for i := 0; i <= offsetsNeeded; i++ {
-        offset := i * 1000
-        qParams := ContractQueryParams{Contract: "nft", Table: nftSymbolUpper + "instances",Query: struct {}{},Limit: 1000, Offset: offset, Index: qParamsIndex}
-        query := herpcQuery{method: "find", params: qParams}
-        queries = append(queries, query)
-    }
+	var queries []herpcQuery
+	for i := 0; i <= offsetsNeeded; i++ {
+		offset := i * 1000
+		qParams := ContractQueryParams{Contract: "nft", Table: nftSymbolUpper + "instances", Query: struct{}{}, Limit: 1000, Offset: offset, Index: qParamsIndex}
+		query := herpcQuery{method: "find", params: qParams}
+		queries = append(queries, query)
+	}
 
-    endpoint := h.Endpoints.Contracts
+	endpoint := h.Endpoints.Contracts
 
-    ress, err := h.rpcExecBatch(endpoint, queries)
-    if err != nil {
-        return nil, err
-    }
+	ress, err := h.rpcExecBatch(endpoint, queries)
+	if err != nil {
+		return nil, err
+	}
 
-    var nfts []EngineNft
-    for _, res := range ress {
-        thisresult := []EngineNft{}
-        if err := json.Unmarshal(res, &thisresult); err != nil { // Parse []byte to the go struct pointer
-            return  nil, err
-        }
-        nfts = append(nfts, thisresult...)
-    }
+	var nfts []EngineNft
+	for _, res := range ress {
+		thisresult := []EngineNft{}
+		if err := json.Unmarshal(res, &thisresult); err != nil { // Parse []byte to the go struct pointer
+			return nil, err
+		}
+		nfts = append(nfts, thisresult...)
+	}
 
-    return nfts, nil
+	return nfts, nil
 }
 
 func (h HiveEngineRpcNode) GetSymbolAllNftFast(nftSymbol string) ([][]byte, error) {
-    if len(h.Endpoints.Contracts) == 0 {
-        h.Endpoints.Contracts = "/contracts"
-    }
-    if h.RpcNode.MaxConn == 0 {
-        h.RpcNode.MaxConn = 1
-    }
-    if h.RpcNode.MaxBatch == 0 {
-        h.RpcNode.MaxBatch = 2
-    }
+	if len(h.Endpoints.Contracts) == 0 {
+		h.Endpoints.Contracts = "/contracts"
+	}
+	if h.RpcNode.MaxConn == 0 {
+		h.RpcNode.MaxConn = 1
+	}
+	if h.RpcNode.MaxBatch == 0 {
+		h.RpcNode.MaxBatch = 2
+	}
 
-    nftSymbolUpper := strings.ToUpper(nftSymbol)
-    collectionSize := h.getSymbolNFTCount(nftSymbolUpper)
-    offsetsNeeded := collectionSize / 1000
+	nftSymbolUpper := strings.ToUpper(nftSymbol)
+	collectionSize, err := h.getSymbolNFTCount(nftSymbolUpper)
+	if err != nil {
+		return nil, err
+	}
+	offsetsNeeded := collectionSize / 1000
 
-    qParamsIndex := []ContractQueryParamsIndex{}
+	qParamsIndex := []ContractQueryParamsIndex{}
 
-    var queries []herpcQuery
-    for i := 0; i <= offsetsNeeded; i++ {
-        offset := i * 1000
-        queryFilter := QueryIDRange{QueryIntRange{offset, offset+999}}
-        qParams := ContractQueryParams{Contract: "nft", Table: nftSymbolUpper + "instances",Query: queryFilter,Limit: 1000, Offset: 0, Index: qParamsIndex}
-        query := herpcQuery{method: "find", params: qParams}
-        queries = append(queries, query)
-    }
+	var queries []herpcQuery
+	for i := 0; i <= offsetsNeeded; i++ {
+		offset := i * 1000
+		queryFilter := QueryIDRange{QueryIntRange{offset, offset + 999}}
+		qParams := ContractQueryParams{Contract: "nft", Table: nftSymbolUpper + "instances", Query: queryFilter, Limit: 1000, Offset: 0, Index: qParamsIndex}
+		query := herpcQuery{method: "find", params: qParams}
+		queries = append(queries, query)
+	}
 
-    endpoint := h.Endpoints.Contracts
+	endpoint := h.Endpoints.Contracts
 
-    ress, err := h.rpcExecBatchFast(endpoint, queries)
-    if err != nil {
-        return nil, err
-    }
+	ress, err := h.rpcExecBatchFast(endpoint, queries)
+	if err != nil {
+		return nil, err
+	}
 
-    return ress, nil
+	return ress, nil
 }
